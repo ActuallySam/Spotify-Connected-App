e4860bfa0821429d24629cddbf7d308b0738d566
ActuallySam <srivastava.samarth.btech2018@sitpune.edu.in>
2021-09-26 23:04:44 +0530

    OAuth Login connected

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+require('dotenv').config()
 const express = require('express');
+const querystring = require('querystring');
 const app = express();
 const port = 8888;
+
+// console.log(process.env.CLIENT_ID);
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
 
 app.get('/', (req, res) => {
     const data = {
@@ -11,11 +19,25 @@ app.get('/', (req, res) => {
     res.json(data);
 });
 
-app.get('/awesome-generator', (req, res) => {
-    const { name, isAwesome } = req.query;
-    res.send(`${name} is ${JSON.parse(isAwesome) ? 'really' : 'not'} awesome`);
-});
+const generateRandomString = length => {
+    let text = '';
+    const possible = 'QWERTYUIOPLKJHGFDSAZXCVBNMqwertyuioplkjhgfdsazxcvbnm1234567890';
+    
+    for (let i=0; i<length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+};
+
+app.get('/login', (req, res) => {
+    const queryParams = querystring.stringify({
+        client_id: CLIENT_ID,
+        response_type: code,
+        redirect_uri: REDIRECT_URI,
+    });
+    res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
+}) 
 
 app.listen(port, () => {
     console.log(`Express app listening at http://localhost:${port}`);
-});
+}); 
