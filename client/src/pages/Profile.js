405be955dba7d8023f26545dbf82d4b4ed4cd3e3
ActuallySam <srivastava.samarth.btech2018@sitpune.edu.in>
2021-10-06 02:55:43 +0530

    Top Tracks and Playlists added

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,13 +1,14 @@
 import { useState, useEffect } from 'react';
 import { catchErrors } from '../utils';
-import { getCurrentUserProfile, getCurrentUserPlaylists, getTopArtists } from '../spotify';
-import { ArtistsGrid, SectionWrapper } from '../components';
+import { getCurrentUserProfile, getCurrentUserPlaylists, getTopArtists, getTopTracks } from '../spotify';
+import { ArtistsGrid, SectionWrapper, TrackList, PlaylistsGrid } from '../components';
 import { StyledHeader } from '../styles';
 
 const Profile = () => {
   const [profile, setProfile] = useState(null);
   const [playlists, setPlaylists] = useState(null);
   const [topArtists, setTopArtists] = useState(null);
+  const [topTracks, setTopTracks] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -19,6 +20,9 @@ const Profile = () => {
 
       const userTopArtists = await getTopArtists();
       setTopArtists(userTopArtists.data);
+      
+      const userTopTracks = await getTopTracks();
+      setTopTracks(userTopTracks.data);
     };
 
     catchErrors(fetchData());
@@ -48,10 +52,18 @@ const Profile = () => {
       </StyledHeader>
       )}
 
-      {topArtists && (
+      {topArtists && topTracks && playlists && (
         <main>
           <SectionWrapper title="Top artists this month" seeAllLink="/top-artists">
             <ArtistsGrid artists={topArtists.items.slice(0, 10)} />
+          </SectionWrapper>
+
+          <SectionWrapper title="Top tracks this month" seeAllLink="/top-tracks">
+            <TrackList artists={topTracks.items.slice(0, 10)} />
+          </SectionWrapper>
+
+          <SectionWrapper title="Playlists" seeAllLink="/playlists">
+            <PlaylistsGrid artists={playlists.items.slice(0, 10)} />
           </SectionWrapper>
         </main>
       )}
--- client/src/spotify.js
+++ client/src/spotify.js
@@ -148,3 +148,13 @@ export const getCurrentUserPlaylists = (limit = 20) => {
 export const getTopArtists = (time_range = 'short_term') => {
     return axios.get(`/me/top/artists?time_range=${time_range}`);
 };
+
+/** 
+ * Get a list of Current Top Tracks
+ * http://developer.spotify.com/documentation/web-api/reference/#edupoint-get-users-top-artists-and-tracks
+ * @param {string} time_range - 'short_term' (last 4 weeks) 'medium_term' (last 6 months) or 'long_term' (calculated from several years of data and including all new data as it becomes available). Defaults to 'short_term'
+ * @returns {Promise}
+*/
+export const getTopTracks = (time_range = 'short_term') => {
+    return axios.get(`/me/top/tracks?time_range=${time_range}`);
+};
