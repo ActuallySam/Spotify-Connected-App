8b52ab859e7290cc427b5326d472f6c992e340b5
ActuallySam <srivastava.samarth.btech2018@sitpune.edu.in>
2021-10-23 22:45:49 +0530

    making it better 3

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -27,7 +27,7 @@ const StyledLoginButton = styled.a`
 const LOGIN_URI = 
   process.env.NODE_ENV !== 'production'
     ? 'http://localhost:8888/login'
-    : 'https://spotify-personal-profile.herokuapp.com/login';
+    : 'https://actuallysam-spotify-profile.herokuapp.com/login';
 
 const Login = () => (
   <StyledLoginContainer>
